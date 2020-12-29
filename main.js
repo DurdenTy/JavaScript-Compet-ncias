e66a162de69fd96737a1c03e6c80d2e7d270ead0
unknown <adilson100-haker@hotmail.com>
2020-12-29 12:33:59 -0300

    Ouvindo Nirvana, fim da parte 1, básica de recapitulação

---
+++ main.js
@@ -0,0 +1,67 @@
+/*var nome = "Adilson de Lima";
+var idade = 34;
+var idade2 = 78;
+var soma = idade + idade2;
+alert(nome + " Você tem " + soma + " anos?");/var frase = "Hmmmmm, apenas recapitulando JavaScript";
+
+console.log(frase.replace("recapitulando" , "revisando"));
+console.log(frase.toUpperCase());*/
+
+
+/*var lista = ["maçã", "pêra", "laranja"];
+lista.push("banana");
+console.log(lista[3]);
+lista.pop();
+console.log(lista[3]);
+console.log(lista.join("fruta: "));*/
+
+/*var objeto = {
+	nome: "Pincel",
+	cor: "Branco",
+	Utilidade: "Limpeza e pintura"
+}
+
+var objeto2 = {
+	nome: "Carro",
+	cor: "Prata",
+	Utilidade: "Andar"
+}
+
+console.log(objeto.nome);
+console.log(objeto.cor);
+console.log(objeto.Utilidade);
+
+console.log(objeto2.nome);*/
+
+/*var objetos = [{ nome: "Pincel", cor: "Branco" }, { nome: "Caneta", cor: "Azul" }]
+console.log(objetos);*/
+
+/*var idade = 18;
+
+if(idade >= 18){
+	alert("Pode participar do show sem os pais");
+}else{
+	alert("Menor de 18 anos, precisa estar acompanhado com um responsável");
+}*/
+/*var i;
+for(i = 0; i <= 5; i++){
+	alert(i);
+}*/
+
+
+
+/*var k = 0;
+do{
+	k++;
+	alert(k);
+}while(k < 5);*/
+
+
+var d = new Date();
+
+alert(d.getHours());
+alert(d.getMinutes());
+alert(d.getSeconds());
+alert(d.getMonth() + 1);
+alert(d.getYear());
+alert(d);
